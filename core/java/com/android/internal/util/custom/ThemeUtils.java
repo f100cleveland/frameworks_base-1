RajaMu/oms_persist_frameworks_base

OMS: Use themes on Power menu
OMS introduced a different way of loading resources into apps, and this new behavior is still buggy. Power menu wasn't getting themed with
our working oms code.

This commit:
 - Picks the following commits from CM
 - Introduces a new helper class: ThemeUtils
 - A lot of name refactoring, mainly uiContext --> mUiContext
 - We can't check (yet) for theme change as CMTE, so I had to drop some if (mContext != null) guards. I didn't notice any performance difference.
 - Both stock and themes should work

Reworked for OMS by @KreAch3R

This is a combination of the following commits:

Show reboot dialog using themed resources: https://github.com/CyanogenMod/android_frameworks_base/commit/d8e138b06abf4147a37d2c3e8f1e95a7e9b52e5c
by 0xD34D
Change-Id: Ia09cccce5beedb1a825ea3730577df97894fe10a

Themes: Theme power off and reboot dialog: https://github.com/CyanogenMod/android_frameworks_base/commit/f7fc9ceefa1e9cad133d694d93f43e3abf18406e
by Altaf-Mahdi
This patch allows theming of the dialog which
comes after the power menu

Change-Id: I81546543da13cf220d9247dbce833d6e883ed091

Distilled changes from these:
https://github.com/CyanogenMod/android_frameworks_base/commit/a4e229c7c09f39ee54df4a150826cab9e435f890
and
https://github.com/CyanogenMod/cm_platform_sdk/blame/cm-13.0/sdk/src/java/org/cyanogenmod/internal/util/ThemeUtils.java

core/java/com/android/internal/util/krexus/ThemeUtils.java
@@ -1,0 +1,60 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2016 Krexus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.krexus;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.pm.PackageManager;
+
+public class ThemeUtils {
+
+    /**
+     * Creates a theme'd context using the overlay applied to SystemUI
+     * @param context Base context
+     * @return Themed context
+     */
+    public static Context createUiContext(final Context context) {
+        try {
+            Context uiContext = context.createPackageContext("com.android.systemui",
+                    Context.CONTEXT_RESTRICTED);
+            return new ThemedUiContext(uiContext, context.getApplicationContext());
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+
+        return null;
+    }
+
+    private static class ThemedUiContext extends ContextWrapper {
+        private Context mAppContext;
+
+        public ThemedUiContext(Context context, Context appContext) {
+            super(context);
+            mAppContext = appContext;
+        }
+
+        @Override
+        public Context getApplicationContext() {
+            return mAppContext;
+        }
+
+        @Override
+        public String getPackageName() {
+            return mAppContext.getPackageName();
+        }
+    }
+}

services/core/java/com/android/server/policy/GlobalActions.java
@@ -21,6 +21,7 @@ import com.android.internal.app.AlertController.AlertParams;
 import com.android.internal.telephony.TelephonyIntents;
 import com.android.internal.telephony.TelephonyProperties;
 import com.android.internal.R;
+import com.android.internal.util.krexus.ThemeUtils;
 import com.android.internal.widget.LockPatternUtils;
 
 import android.app.ActivityManager;
@@ -106,6 +107,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
     private final Context mContext;
     private final WindowManagerFuncs mWindowManagerFuncs;
+    private Context mUiContext;
     private final AudioManager mAudioManager;
     private final IDreamManager mDreamManager;
 
@@ -211,6 +213,12 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         }
     }
 
+   private Context getUiContext() {
+        mUiContext = ThemeUtils.createUiContext(mContext);
+        mUiContext.setTheme(android.R.style.Theme_DeviceDefault_Light_DarkActionBar);
+        return mUiContext != null ? mUiContext : mContext;
+    }
+
     /**
      * Create the global actions dialog.
      * @return A new dialog.
@@ -312,12 +320,12 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         mAdapter = new MyAdapter();
 
-        AlertParams params = new AlertParams(mContext);
+        AlertParams params = new AlertParams(getUiContext());
         params.mAdapter = mAdapter;
         params.mOnClickListener = this;
         params.mForceInverseBackground = true;
 
-        GlobalActionsDialog dialog = new GlobalActionsDialog(mContext, params);
+        GlobalActionsDialog dialog = new GlobalActionsDialog(getUiContext(), params);
         dialog.setCanceledOnTouchOutside(false); // Handled by the custom class.
 
         dialog.getListView().setItemsCanFocus(true);
@@ -707,7 +715,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         public View getView(int position, View convertView, ViewGroup parent) {
             Action action = getItem(position);
-            return action.create(mContext, convertView, parent, LayoutInflater.from(mContext));
+            final Context context = getUiContext();
+            return action.create(context, convertView, parent, LayoutInflater.from(context));
         }
     }
 

f100cleveland/frameworks_base-1

core/java/com/android/internal/util/custom/ThemeUtils.java
@@ -1,0 +1,60 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2016 Krexus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.custom;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.pm.PackageManager;
+
+public class ThemeUtils {
+
+    /**
+     * Creates a theme'd context using the overlay applied to SystemUI
+     * @param context Base context
+     * @return Themed context
+     */
+    public static Context createUiContext(final Context context) {
+        try {
+            Context uiContext = context.createPackageContext("com.android.systemui",
+                    Context.CONTEXT_RESTRICTED);
+            return new ThemedUiContext(uiContext, context.getApplicationContext());
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+
+        return null;
+    }
+
+    private static class ThemedUiContext extends ContextWrapper {
+        private Context mAppContext;
+
+        public ThemedUiContext(Context context, Context appContext) {
+            super(context);
+            mAppContext = appContext;
+        }
+
+        @Override
+        public Context getApplicationContext() {
+            return mAppContext;
+        }
+
+        @Override
+        public String getPackageName() {
+            return mAppContext.getPackageName();
+        }
+    }
+}

services/core/java/com/android/server/policy/GlobalActions.java
@@ -21,6 +21,7 @@ import com.android.internal.app.AlertController.AlertParams;
 import com.android.internal.telephony.TelephonyIntents;
 import com.android.internal.telephony.TelephonyProperties;
 import com.android.internal.R;
+import com.android.internal.util.custom.ThemeUtils;
 import com.android.internal.widget.LockPatternUtils;
 
 import android.app.ActivityManager;
@@ -105,6 +106,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
     private final Context mContext;
     private final WindowManagerFuncs mWindowManagerFuncs;
+    private Context mUiContext;
     private final AudioManager mAudioManager;
     private final IDreamManager mDreamManager;
 
@@ -210,6 +212,12 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         }
     }
 
+    private Context getUiContext() {
+        mUiContext = ThemeUtils.createUiContext(mContext);
+        mUiContext.setTheme(android.R.style.Theme_DeviceDefault_Light_DarkActionBar);
+        return mUiContext != null ? mUiContext : mContext;
+    }
+
     /**
      * Create the global actions dialog.
      * @return A new dialog.
@@ -311,12 +319,12 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         mAdapter = new MyAdapter();
 
-        AlertParams params = new AlertParams(mContext);
+        AlertParams params = new AlertParams(getUiContext());
         params.mAdapter = mAdapter;
         params.mOnClickListener = this;
         params.mForceInverseBackground = true;
 
-        GlobalActionsDialog dialog = new GlobalActionsDialog(mContext, params);
+        GlobalActionsDialog dialog = new GlobalActionsDialog(getUiContext(), params);
         dialog.setCanceledOnTouchOutside(false); // Handled by the custom class.
 
         dialog.getListView().setItemsCanFocus(true);
@@ -696,7 +704,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         public View getView(int position, View convertView, ViewGroup parent) {
             Action action = getItem(position);
-            return action.create(mContext, convertView, parent, LayoutInflater.from(mContext));
+            final Context context = getUiContext();
+            return action.create(context, convertView, parent, LayoutInflater.from(context));
         }
     }
 